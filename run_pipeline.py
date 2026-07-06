ae5e443f4f4847a943605bdb2e545668333bfd0e
konstantinlokotunin <konstantin.lokotunin@sciencespo.fr>
2026-07-06 20:31:46 +0200

    Remove interim, processed, and raw Python files; add industrial releases of pollutants to air dataset.

--- run_pipeline.py
+++ run_pipeline.py
@@ -4,7 +4,7 @@ from src.transform import transform_data
 
 def main():
 
-    df = extract_data(r"""C:\Users\konst\Documents\Python Projects\ESG Data Sentinel\esg-data-sentinel\data\industrial_releases_of_pollutants_to_air.csv""")
+    df = extract_data(r"""C:\Users\konst\Documents\Python Projects\ESG Data Sentinel\esg-data-sentinel\data\raw\industrial_releases_of_pollutants_to_air.csv""")
     df = transform_data(df)
 
     print("\nFirst rows:")
