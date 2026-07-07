3663e885f0de12386f0e30e79ed6d6ceb26690a5
konstantinlokotunin <konstantin.lokotunin@sciencespo.fr>
2026-07-07 15:59:52 +0200

    Add error handling for data extraction and improve CSV loading logic

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,11 +1,13 @@
-from src.extract import extract_data
+from src.extract import extract_data, InvalidFileFormat
 from src.transform import transform_data
 
 
 def main():
 
-    df = extract_data(r"""C:\Users\konst\Documents\Python Projects\ESG Data Sentinel\esg-data-sentinel\data\raw\industrial_releases_of_pollutants_to_air.csv""")
-    df = transform_data(df)
+    try:
+        df = extract_data(r"""C:\Users\konst\Documents\Python Projects\ESG Data Sentinel\esg-data-sentinel\data\raw\industrial_releases_of_pollutants_to_air.csv""")
+    except InvalidFileFormat as e:
+        print(e)
 
     print("\nFirst rows:")
     print(df.head())
--- src/extract.py
+++ src/extract.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 import pandas as pd
 
+class InvalidFileFormat(Exception):
+    pass
+
 def extract_data(file_path):
     """
     Loads one CSV or Excel file into a pandas DataFrame.
@@ -11,13 +14,13 @@ def extract_data(file_path):
     print("Loading file:", file_path.name)
 
     if file_path.suffix.lower() == ".csv":
-        df = pd.read_csv(file_path, low_memory=False)
+        df = pd.read_csv(file_path, sep=";", low_memory=False)
 
     elif file_path.suffix.lower() in [".xlsx", ".xls"]:
         df = pd.read_excel(file_path)
 
     else:
-        raise ValueError("Only CSV and Excel files are supported.")
+        raise InvalidFileFormat("Only CSV and Excel files are supported.")
 
     print("Data loaded successfully.")
     print("Shape:", df.shape)
